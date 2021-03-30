a960f042a370882ef460dbc916f4392e140332c9
CerealYeti <geddypence@gmail.com>
2021-03-30 23:38:28 +0000

    started on evil config

--- init.el
+++ init.el
@@ -14,6 +14,9 @@
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
+;;Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 ;; disable line numbers for certain modes
 (dolist (mode '(org-mode-hook
 		term-mode-hook
@@ -26,12 +29,6 @@
 
 ;; set fontv
 (set-face-attribute 'default nil :font "Fira Code Retina 12")
-
-;; load theme
-(load-theme 'wombat)
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Initialize package sources
 (require 'package)
@@ -50,7 +47,7 @@
   (package-install 'use-package))
 
 (require 'use-package)
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t):
 
 (use-package ivy
   :diminish
@@ -58,14 +55,14 @@
 	 :map ivy-minibuffer-map
 	 ("TAB" . ivy-alt-done)
 	 ("C-l" . ivy-alt-done)
-	 ("C-j" . ivy-next-line)
+ 	 ("C-j" . ivy-next-line)
 	 ("C-k" . ivy-previous-line)
 	 :map ivy-switch-buffer-map
 	 ("C-k" . ivy-previous-line)
 	 ("C-l" . ivy-done)
 	 ("C-d" . ivy-switch-buffer-kill)
 	 :map ivy-reverse-i-search-map
-	 ("C-k" . ivy-previous-line)
+ 	 ("C-k" . ivy-previous-line)
 	 ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
@@ -86,15 +83,23 @@
   :init (doom-modeline-mode 1))
 
 (use-package doom-themes
+  :init
+  (load-theme 'doom-dracula t)
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t
-	doom-themes-enable-italic t) ;; enable bold and italic universally
+	doom-themes-enable-italic t) ;; enable bold and italic universallyva
   (doom-themes-visual-bell-config)
   (doom-themes-org-config))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+
+;; NOTE: The first time you load your configuration on a new machine, you'll need to run the following
+;; command intereactively so that mode line icons display correctly:
+;;
+;; M-x all-the-icons-install-fonts
+(use-package all-the-icons)
 
 (use-package which-key
   :init (which-key-mode)
@@ -114,3 +119,63 @@
   ([remap describe-command] . helpful-command)
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (hydra evil-collection evil general helpful ivy-rich which-key rainbow-delimiters doom-themes doom-modeline counsel ivy use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rune/leader-keys
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")))
+
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; User visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
